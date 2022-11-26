c7de51618bc456f73fbdb76fa7a9e189a74cd137
BorosFather <boros.zoli024@gmail.com>
2022-11-26 17:52:42 +0100

    Már megin nem müködik nirmálisan a git

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,15 @@
+/*
+* File: app.component.ts
+* Author: Boros Zoltán
+* Copyright: 2022, Boros Zoltán
+* Group: Szoft II N
+* Date: 2022-11-26
+* Github: https://github.com/BorosFather
+* Licenc: GNU GPL
+*/
+
 import { Component } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +18,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'telteg';
+
+  aSide: number = 0;
+  bSide: number = 0;
+  area!: number;
+  hideArea = false;
+  hiddenbutt = true;
+
+  calcHandle():void{
+    if(this.checkValid()){
+      this.area = this.calcArea(this.aSide, this.bSide);
+      this.aSide = 0;
+      this.bSide = 0;
+      this.hideArea = true;
+    }
+    else{
+     alert("Érvénytelen bemenő adatok!")
+    }
+  }
+
+  calcArea(aSide:number, bSide:number):number{
+    let area = (aSide * bSide) * 2;
+    return area;
+  }
+
+  checkValid():boolean{
+  let valid = true;
+ if(this.aSide <=0){
+  valid = false;
+ }
+ if(this.bSide <=0){
+  valid = false;
+ }
+ return valid;
+  }
+
+  hidden():void{
+    this.hideArea = true;
+  }
+
 }
